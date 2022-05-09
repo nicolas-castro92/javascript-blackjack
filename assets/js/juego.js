d7fc6dc9f8606f996ab8a834691ff9834e392e89
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2022-05-08 21:25:05 -0500

    funcion valor carta con la funcion isNaN ternaria

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -20,9 +20,48 @@ const crearDeck = () => {
     }
 
     console.log(deck);
-    const variable = _.shuffle(deck)
-    console.log(variable);
+    deck = _.shuffle(deck)
+    console.log(deck);
+    return deck;
 }
 
 crearDeck();
 
+const pedirCarta = () => {
+
+    if( deck.length === 0 ){
+        throw 'No hay mas cartas en la baraja';
+    }
+
+    const carta = deck.pop();
+    console.log(carta);
+    console.log(deck);
+
+    return carta;
+}
+//let valoresAceptados = pedirCarta().replace(/\D/g,'');
+pedirCarta();
+
+const valorCarta = (carta) => {
+    
+    const valor = carta.substring(0,carta.length-1);
+    
+    return ( isNaN(valor) ) ? 
+            ( valor==='A' ) ? 11 : 10 
+            : valor * 1 ;
+
+    /* let puntos  = 0;
+    console.log('dime'+valor);
+
+    if( isNaN(valor) ){
+        puntos = (valor==='A') ? 11 : 10;
+        console.log(puntos);
+    }else{
+        puntos = valor*1;
+        console.log(puntos);
+    } */
+
+}
+
+const resultado = valorCarta(pedirCarta());
+console.log({resultado});
